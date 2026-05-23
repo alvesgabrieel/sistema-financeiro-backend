837db2259a9ad11493ab48249ed0073ace5d39cf
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2026-05-23 10:23:45 -0300

    :sparkles: feat: database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [    
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
